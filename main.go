67680b2f7d88fe4f29abb8aa6623207b30ec4579
Joseph Bouqdib <joseph@bouqdib.com>
2025-03-16 16:56:51 +0000

    implement interactive input loop for Pokedex CLI

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		cleaned := cleanInput(input)
+		fmt.Println("Your command was:", cleaned[0])
+	}
 }
